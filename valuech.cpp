8cf67cfb31742126c34fa259f62cd26534e8cd16
AhsanKhan321 <ahsannu2@gmail.com>
2020-08-07 13:16:30 -0400

    code changes

--- valuech.cpp
+++ valuech.cpp
@@ -5,13 +5,14 @@ int main()
     int n=0;
     cout<<"Enter value of n = ";
     cin>>n;
-    if (n>=0 & n<=20)
+    while (n>20)
     {
-        cout<<"Answer of N+17 = "<<n+17<<endl;
+      cout<<"Error"<<endl;
+        cout<<"Enter value of n = ";
+    cin>>n;
     }
-    else
     {
-       cout<<"Error"<<endl;
+      cout<<"Answer of N+17 = "<<n+17<<endl;
     }
 
 }
