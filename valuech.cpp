8b2b00bebd0c88c2d4ca59b113f931c637d8bf5d
AhsanKhan321 <ahsannu2@gmail.com>
2020-08-07 13:08:00 -0400

    Value check

---
+++ valuech.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n=0;
+    cout<<"Enter value of n = ";
+    cin>>n;
+    if (n>=0 & n<=20)
+    {
+        cout<<"Answer of N+17 = "<<n+17<<endl;
+    }
+    else
+    {
+       cout<<"Error"<<endl;
+    }
+
+}
